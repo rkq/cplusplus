cf3f635e8db98731e7b6d526872d96ddacf92105
Rick Qiu <qiudejun@gmail.com>
2017-08-12 11:46:26 +0800

    Reorg structure

---
+++ src/cplusplus/algods/reorder.h
@@ -0,0 +1,27 @@
+//
+// Copyright (c) 2017 Rick Qiu. All rights reserved.
+//
+#ifndef CPLUSPLUS_ALGODS_REORDER_H_
+#define CPLUSPLUS_ALGODS_REORDER_H_
+
+#include <algorithm>
+
+namespace cplusplus {
+template<typename RandomIt, typename UnaryPredicate>
+  void Reorder(RandomIt first, RandomIt last, UnaryPredicate pred) {
+    RandomIt forward = first;
+    RandomIt backward = last - 1;
+    while (forward < backward) {
+      while (forward < backward && pred(*forward)) {
+        ++forward;
+      }
+      while (forward < backward && !pred(*backward)) {
+        --backward;
+      }
+      if (forward < backward) {
+        std::iter_swap(forward, backward);
+      }
+    }
+  }
+}  // namespace cplusplus
+#endif  // CPLUSPLUS_ALGODS_REORDER_H_
--- src/cplusplus/algods/tests/reorder_test.cc
+++ src/cplusplus/algods/tests/reorder_test.cc
@@ -1,16 +1,21 @@
-#include "gtest/gtest.h"
+#include "reorder.h"
+
 #include <vector>
 
-void Reorder(std::vector<int>& v);
+#include "gtest/gtest.h"
+
+bool IsOdd(int a) {
+  return a % 2 != 0;
+}
 
 TEST(TestReorder, NormalTest) {
   std::vector<int> v({1, 2, 3, 4, 5, 6, 7, 8, 9});
-  Reorder(v);
+  cplusplus::Reorder(v.begin(), v.end(), IsOdd);
   EXPECT_EQ(v, std::vector<int>({1, 9, 3, 7, 5, 6, 4, 8, 2}));
 }
 
 TEST(TestReorder, NoOperation) {
   std::vector<int> v({1, 3, 5, 7, 9, 2, 4, 6, 8});
-  Reorder(v);
+  cplusplus::Reorder(v.begin(), v.end(), IsOdd);
   EXPECT_EQ(v, std::vector<int>({1, 3, 5, 7, 9, 2, 4, 6, 8}));
 }
